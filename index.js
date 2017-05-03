996cd4fbafbff0688cf3f57884b51f6738b10752
nickiitsu <pannatorn.po@skru.ac.th>
2017-05-03 14:51:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
-app.use(express.static('public'))
 
-var server = app.listen(3000 , function(){
-		var host = server.address().address
-		var port = server.address().port
-		console.log('app listing :' , host , port)
+app.use(bodyParser.json())
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.get('/', function (req, res) {
+	res.send('Hello')
+})
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
 })
